fa06da283e2dc3f04eb672b4508507454fd66cb9
Stinger <stinger@int.pl>
2022-01-16 21:08:08 +0100

    some more adjustments for mobile view and finished share button

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const shareBtn = document.querySelector("button")
 const mediaBar = document.querySelector(".avatar-wrap")
 const avatar = document.querySelector(".avatar")
 const txtContainer = document.querySelector(".text-container")
+const socialMedia = document.querySelector(".social-media")
 
 shareBtn.addEventListener("click", () => {
     mediaBar.classList.toggle("active")
     avatar.classList.toggle("active")
     txtContainer.classList.toggle("active")
     shareBtn.classList.toggle("active")
+    socialMedia.classList.toggle("active")
 })
